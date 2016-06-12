cef1a5c71785aff047b438d08f49cafffc48c0a7
Sarah Doran <sarah.doran@mttnow.com>
2016-06-12 01:44:56 +0100

    Remove broken unnecessary loader

--- webpack.config.js
+++ webpack.config.js
@@ -37,12 +37,7 @@ module.exports = {
 			{ test: /\.css$/, loader: 'style!css!postcss' },
 			{ test: /\.scss$/, loader: 'style!css!postcss!sass' },
 			{ test: /\.(jpg|jpeg|png|gif|svg|ico)$/i, loader:'file?name=img/[name].[ext]' },
-			{ test: /\.(otf|eot|svg|ttf|woff|woff2)/i, loader : 'file?name=font/[name]/[name].[ext]' },
-      { test: /\.svg$/, loader: 'url?limit=65000&mimetype=image/svg+xml&name=public/fonts/[name].[ext]' },
-      { test: /\.woff$/, loader: 'url?limit=65000&mimetype=application/font-woff&name=public/fonts/[name].[ext]' },
-      { test: /\.woff2$/, loader: 'url?limit=65000&mimetype=application/font-woff2&name=public/fonts/[name].[ext]' },
-      { test: /\.[ot]tf$/, loader: 'url?limit=65000&mimetype=application/octet-stream&name=public/fonts/[name].[ext]' },
-      { test: /\.eot$/, loader: 'url?limit=65000&mimetype=application/vnd.ms-fontobject&name=public/fonts/[name].[ext]' }
+			{ test: /\.(otf|eot|svg|ttf|woff|woff2)/i, loader : 'file?name=font/[name]/[name].[ext]' }
 		],
 		postcss: [
 			AutoPrefixer({browsers: [
